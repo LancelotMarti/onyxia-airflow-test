d49aa6ac4808cd0926ce1ddae98d5f77301984e7
LancelotMarti <70134056+LancelotMarti@users.noreply.github.com>
2026-02-09 12:53:26 +0100

    test

--- dags/test.py
+++ dags/test.py
@@ -6,11 +6,11 @@ def print_hello():
     print("HELLO")
 
 dag = DAG(
-    "hello_dag",
-    description = "Simple DAG",
-    schedule_interval = "0 0 * * *",
-    start_date = datetime(2023,1,1),
-    catchup = False
+    dag_id="hello_dag",
+    description="Simple DAG",
+    schedule="0 0 * * *",
+    start_date=datetime(2023, 1, 1),
+    catchup=False
 )
 
 print_hello_task = PythonOperator(
