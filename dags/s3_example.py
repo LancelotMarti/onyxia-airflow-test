24f3a7e31844045b3c96dc2056e49e14c15ae779
LancelotMarti <70134056+LancelotMarti@users.noreply.github.com>
2026-03-27 09:57:16 +0100

    Refactor DAG to define read_csv_from_s3 task

--- dags/s3_example.py
+++ dags/s3_example.py
@@ -1,13 +1,31 @@
+from airflow import DAG
+from airflow.decorators import task
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from datetime import datetime
+import pandas as pd
+from io import StringIO
 
-@task
-def read_csv_from_s3():
-    hook = S3Hook(aws_conn_id="aws_onyxia")
-
-    obj = hook.get_key(
-        key="airflow/titanic.csv",
-        bucket_name="lmarti"
-    )
-
-    df = pd.read_csv(obj.get()["Body"])
-    print(df.head())
+# ---------------------------
+# DAG definition
+# ---------------------------
+with DAG(
+    dag_id="read_csv_s3_head",
+    start_date=datetime(2026, 3, 27),
+    schedule=None,  # manual trigger
+    catchup=False,
+    tags=["example", "s3", "minio"],
+) as dag:
+    @task
+    def read_csv_from_s3():
+        hook = S3Hook(aws_conn_id="aws_onyxia")
+    
+        obj = hook.get_key(
+            key="airflow/titanic.csv",
+            bucket_name="lmarti"
+        )
+    
+        df = pd.read_csv(obj.get()["Body"])
+        print(df.head())
+        
+    # Define task execution
+    read_csv_from_s3()
